89f132656cefdd5fcfb1299d20ba91009b21d0c2
Farid <makflay85zet@gmail.com>
2023-07-24 02:45:04 +0400

    added Test01.

--- src/main/java/Lesson1/Test01.java
+++ src/main/java/Lesson1/Test01.java
@@ -3,10 +3,12 @@ package Lesson1;
 public class Test01 {
     public static void main(String[] args) {
         int x = 0;
-        if(x > 0 ){
+        if (x > 0) {
             System.out.println("x>0");
-        }else {
-            RuntimeException error = new RuntimeException("X<=0");
+        } else if (x == 0) {
+            throw new RuntimeException("x==0");
+        } else {
+            RuntimeException error = new RuntimeException("X<0");
             System.err.println(error.toString());
             System.err.println(error.getMessage());
             System.err.println(error.getCause());
